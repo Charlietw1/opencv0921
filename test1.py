de1eda2810d661a59eb81de58458da913590d96b
cflin <cflin@mail.cjcu.edu.tw>
2022-09-21 09:54:31 +0800

    test111

--- test1.py
+++ test1.py
@@ -6,6 +6,6 @@ img = cv2.imread("./test1.png",0)     #(flag = 0 or 1 or -1)
 
 #Import only if not previously imported
 import cv2
-cv2.imshow("Wi121212112me", img)
+cv2.imshow("Wi1212112122112me", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
