e75d259d3251f51fdae4254cec3e20f3ecb3d7e5
cflin <cflin@mail.cjcu.edu.tw>
2022-09-21 09:42:58 +0800

    first commit

---
+++ test1.py
@@ -0,0 +1,11 @@
+import cv2
+
+#Import only if not previously imported
+import cv2
+img = cv2.imread("./test1.png",0)     #(flag = 0 or 1 or -1)
+
+#Import only if not previously imported
+import cv2
+cv2.imshow("Window Name", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
